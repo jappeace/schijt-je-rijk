0500e342133d740fbc43b0c63c7b3d37287e39c6
Jappie Klooster <jappieklooster@hotmail.com>
2016-11-27 09:00:46 +0100

    added a simple html based graphics

---
+++ Main.elm
@@ -0,0 +1,78 @@
+-- elm-lang/core
+import Task
+import Color exposing (linear, rgb)
+
+-- elm-lang/html
+import Html exposing (..)
+
+-- evancz/elm-graphics
+import Collage exposing (collage, gradient, rect)
+import Element exposing (toHtml)
+
+-- elm-lang/window
+import Window exposing (..)
+
+
+main =
+    program 
+      { init = init
+      , view = view
+      , update = update
+      , subscriptions = subscriptions
+      }
+
+-- Model
+
+type alias Model = 
+    { size : Window.Size
+    }
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model ( Window.Size 0 0 ), Task.perform (\x -> Resize x) Window.size )
+
+
+type Msg
+    = Resize Window.Size
+    | Fail
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Resize newSize ->
+            ( Model newSize, Cmd.none )
+        Fail ->
+            ( model, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch [ Window.resizes Resize ]
+
+
+view : Model -> Html Msg
+view model =
+    let
+        w =
+            model.size.width
+
+        h =
+            model.size.height
+
+        clrStart =
+            rgb 5 250 140
+
+        clrEnd =
+            rgb 231 59 87
+
+        clrStops =
+            [ ( 0.0, clrStart ), ( 1.0, clrEnd ) ]
+
+        gfx =
+            collage w h 
+                [ gradient (linear ( 0, 0 ) ( toFloat w, toFloat h ) clrStops) (rect (toFloat w) (toFloat h)) ]
+    in
+  body [] [
+    h1 [] [text "Schijt je rijk"], 
+    toHtml gfx
+  ]
+  
