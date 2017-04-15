a218111fc02e533e357e51c357ad740ac5476dae
Jappie Klooster <jappieklooster@hotmail.com>
2017-04-15 23:00:10 +0200

    Added a release script

---
+++ release.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+mkdir release
+cp -R img release/img
+elm-make Main.elm --output=release/start.html
+cp LICENSE release/LICENSE.txt
+cp readme.org release/readme.org
+# just to make sure windows people can open it
+cp readme.org release/leesmij.txt
+
+tar -czvf release.tar.gz release
